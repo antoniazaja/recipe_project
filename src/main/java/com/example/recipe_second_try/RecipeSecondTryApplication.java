df8726d6915ed0119b1de8d482eabd1d67bbb8ff
Antonia Žaja <tonka.zaja@fer.hr>
2022-09-06 15:15:54 +0200

    #1

---
+++ src/main/java/com/example/recipe_second_try/RecipeSecondTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipe_second_try;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSecondTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeSecondTryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipe_second_try/RecipeSecondTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe_second_try;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSecondTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
